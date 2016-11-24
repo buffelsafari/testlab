605418d98660a83b3bd16bc37d98b9f86f35f204
buffel <buffel@buffel-Dator>
2016-11-24 08:15:03 +0100

    commit 2

--- src/testcase1.java
+++ src/testcase1.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-
+//gjh
 public class testcase1
 {
 	Splorf sut;
 	@Test
 	public void test() 
 	{
+		// an unexpected change 
+		
 		int expected=1;
 		int actual;
 		sut=new Splorf();
